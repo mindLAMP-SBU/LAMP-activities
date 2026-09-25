97eb6fd852e36f7a36b221750a82bb5342766628
Kacper Letowski <kacperlet27@gmail.com>
2026-09-25 01:45:53 -0400

    updated the resolution of the situational-awareness activity on PC

--- situational-awareness/src/app/AppLayout.jsx
+++ situational-awareness/src/app/AppLayout.jsx
@@ -2,11 +2,30 @@
 import { Box, Paper } from "@mui/material";
 import Navbar from "../components/Navbar";
 
+// Max width of the app column; on wider screens (PC) the app is centered in a phone-shaped column
+const MOBILE_MAX_WIDTH = 480;
+
 export default function AppLayout({ children }) {
 
   return (
-    <Box display="flex" height="100vh">
-      <Box flex={1} display="flex" flexDirection="column">
+    <Box
+      display="flex"
+      justifyContent="center"
+      minHeight="100vh"
+      sx={{ bgcolor: { xs: "background.paper", sm: "grey.200" } }}
+    >
+      <Box
+        flex={1}
+        display="flex"
+        flexDirection="column"
+        minHeight="100vh"
+        sx={{
+          width: "100%",
+          maxWidth: MOBILE_MAX_WIDTH,
+          bgcolor: "background.paper",
+          boxShadow: { xs: "none", sm: 6 },
+        }}
+      >
         <Navbar />
         <Box display="flex" component="main" flex={1} overflow="visible" flexDirection="column">
           <Paper
